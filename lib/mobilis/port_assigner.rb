5b4b2409f6c9ce0d20d69ea5ad6d0d22022f3941
Meleneth <meleneth@gmail.com>
2025-03-26 04:46:26 -0700

    [rbs] the end is in sight

--- lib/mobilis/port_assigner.rb
+++ lib/mobilis/port_assigner.rb
@@ -4,15 +4,15 @@ module Mobilis
   class PortAssigner
     include Singleton
 
-    def initialize(starting_port_no: 10000, port_gap: 100)
-      @assigned_ports = {}
+    def initialize(starting_port_no: 10_000, port_gap: 100)
+      @assigned_ports = {} # : Hash[String, Integer]
       @starting_port_no = starting_port_no
       @port_gap = port_gap
       @offset = 0
     end
 
     def for_project(project)
-      @assigned_ports[project.name] ||= next_port_number_to_assign :project
+      @assigned_ports[project.name] ||= next_port_number_to_assign
     end
 
     private
