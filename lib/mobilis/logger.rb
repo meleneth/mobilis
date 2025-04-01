bdb45602c63c51c39dc0099722e95aa8186fd70e
Lord Meleneth <meleneth@gmail.com>
2025-04-01 15:22:17 -0700

    Bring logger back

---
+++ lib/mobilis/logger.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require "logger"
+module Mobilis
+  class << self
+    def logger
+      @logger ||= Logger.new($stdout).tap do |log|
+        log.progname = "mobilis"
+        log.level = Logger::DEBUG
+      end
+    end
+  end
+end
